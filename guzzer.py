1d031f1e9b25cd72b742f50f0fecd6c8ade37446
ghostinthefingers <ghostinthefingers@tutanota.com>
2021-09-25 06:24:08 +0330

    removed not found text

--- guzzer.py
+++ guzzer.py
@@ -5,9 +5,8 @@ from spider import spider
 
 parser = argparse.ArgumentParser(description='Guzzer')
 parser.add_argument('-u','--url', help='base url of the page', required=True)
-parser.add_argument('-n','--notfound', help='a pice of not found page.for example:this page not found', required=True)
 args = vars(parser.parse_args())
 
-spider(args['url'],args['notfound'])
+spider(args['url'])
 
 
--- spider.py
+++ spider.py
@@ -1,11 +1,15 @@
 import aiohttp
 import asyncio
 import time
+import requests
 
-
-def spider(base_url,not_found_text):
+def spider(base_url,):
     urls = []
     if base_url[-1] != '/': base_url += '/'  #append "/" to the end of the base url
+
+    not_found = requests.get(base_url+"something_that_shows_this_is_a_not_found_page")
+    not_found_text = not_found.text
+
     print('finding interesting files...')
     with open('common.txt','r') as wordlist:
         for w in wordlist:
@@ -16,8 +20,8 @@ def spider(base_url,not_found_text):
     async def get_url(session, url):
         async with session.get(url) as resp:
             resp_text = await resp.text()
-            if not_found_text not in resp_text :
-                print('find this url, might be interesting:  '+ url)
+            if not_found_text != resp_text:
+                print('found this url, might be interesting:  '+ url)
 
     async def main():
 
